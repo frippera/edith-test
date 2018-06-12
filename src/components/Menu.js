f820bb7f8bff6a77868129feb230c218a134469d
Anders Friborg <frippera@gmail.com>
2018-06-12 19:56:12 +0200

    Added Mobile Navigation

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,38 +2,64 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { FaBars, FaHeartO, FaHeart } from 'react-icons/lib/fa';
 
-const Favorites = [{"count":"1"}];
+const Favorites = [];
 
 class Menu extends Component {
+	handleMenuMobile = () => {
+		let menuMobile = document.querySelector('.MenuMobile');
+
+		if (menuMobile.style.display === 'flex') {
+			menuMobile.style.display = 'none';
+		} else {
+			menuMobile.style.display = 'flex';
+		}
+	}
+
 	render() {
 		return (
-			<div className="Menu">
-				<div className="MenuItem">
-					<span className="MenuLinkMobile"><FaBars size={16} /></span>&nbsp;
+			<div>
+				<div className="Menu">
+					<div className="MenuItem">
+						<span className="MenuLinkMobile" onClick={this.handleMenuMobile}><FaBars size={16} /></span>&nbsp;
+					</div>
+					<div className="MenuItem">
+						<Link to="/" className="MenuLink">
+							Hem
+						</Link>
+						<Link to="/lunch" className="MenuLink">
+							Veckans lunch
+						</Link>
+						<Link to="/">
+							<img src="logo.png" className="Logo" alt="Ediths Kök" />
+						</Link>
+						<Link to="/catering" className="MenuLink">
+							Catering
+						</Link>
+						<Link to="/bakery" className="MenuLink">
+							Bageri
+						</Link>
+					</div>
+					<div className="MenuItem">
+						<Link to="/favorites" className="MenuLink">
+							Favoriter {Favorites.length ? <FaHeart className="MenuLinkIcon" size={16} /> : <FaHeartO className="MenuLinkIcon" size={16} /> }
+						</Link>
+						<Link to="/favorites" className="MenuLinkFavorite">
+							{Favorites.length ? <FaHeart className="MenuLinkIcon" size={16} /> : <FaHeartO className="MenuLinkIcon" size={16} /> }
+						</Link>
+					</div>
 				</div>
-				<div className="MenuItem">
-					<Link to="/" className="MenuLink">
+				<div className="MenuMobile">
+					<Link to="/" className="MenuMobileLink">
 						Hem
 					</Link>
-					<Link to="/lunch" className="MenuLink">
+					<Link to="/lunch" className="MenuMobileLink">
 						Veckans lunch
 					</Link>
-					<Link to="/">
-						<img src="logo.png" className="Logo" alt="Ediths Kök" />
-					</Link>
-					<Link to="/catering" className="MenuLink">
+					<Link to="/catering" className="MenuMobileLink">
 						Catering
 					</Link>
-					<Link to="/bakery" className="MenuLink">
+					<Link to="/bakery" className="MenuMobileLink">
 						Bageri
-					</Link>
-				</div>
-				<div className="MenuItem">
-					<Link to="/favorites" className="MenuLink">
-						Favoriter {Favorites.length ? <FaHeartO className="MenuLinkIcon" size={16} /> : <FaHeart className="MenuLinkIcon" size={16} /> }
-					</Link>
-					<Link to="/favorites" className="MenuLinkFavorite">
-						{Favorites.length ? <FaHeartO className="MenuLinkIcon" size={16} /> : <FaHeart className="MenuLinkIcon" size={16} /> }
 					</Link>
 				</div>
 			</div>
